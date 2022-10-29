81357c9e9ee0b731bd1872d9243c26f469869c14
Максим Новожилов <intention-m26@mail.ru>
2022-10-29 22:22:20 +0300

    Updated commit

--- src/main/java/ru/netology/repository/NotFoundException.java
+++ src/main/java/ru/netology/repository/NotFoundException.java
@@ -1,2 +1,8 @@
-public class NotFoundException {
+package ru.netology.repository;
+
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(String s) {
+        super(s);
+    }
 }
